e216258da1fec6139fb37dbd6dab459255f6eda9
Crashk1 <crashkofslug@gmail.com>
2020-11-06 03:10:08 -0300

    preparando Jugador e incluyendo Herencia

--- src/principal/Jugador.java
+++ src/principal/Jugador.java
@@ -2,4 +2,12 @@ package principal;
 
 public class Jugador {
 
+	// Me hago cargo yo (Alexis) de Jugador
+	private Double salario;
+	private Integer numero;
+	private Integer faltas;
+	private Integer partidosGanados;
+	private Integer campeonatosGanados;
+	
+	
 }
---
+++ src/principal/JugadorFutbol.java
@@ -0,0 +1,11 @@
+package principal;
+
+public class JugadorFutbol extends Jugador {
+	
+	private Integer goles;
+	private Integer tirosAlArco;
+	private Integer tiros;
+	private Integer posicion;
+	
+	
+}
