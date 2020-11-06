cd686f980d19af1d06746eda6a0af44e8624e4fc
Crashk1 <crashkofslug@gmail.com>
2020-11-06 14:43:35 -0300

    agregando un ArrayList a Jugador

--- src/principal/JugadorFutbol.java
+++ src/principal/JugadorFutbol.java
@@ -1,11 +1,14 @@
 package principal;
 
+import java.util.ArrayList;
+
 public class JugadorFutbol extends Jugador {
 	
 	private Integer goles;
 	private Integer tirosAlArco;
 	private Integer tiros;
 	private Integer posicion;
+	private ArrayList <Partido> partidosJugados = new ArrayList <Partido>();
 	
 	
 }
