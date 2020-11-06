4663f34a62a36b8e6d2bb45734d68dcbfd9cf78d
Crashk1 <crashkofslug@gmail.com>
2020-11-06 02:08:24 -0300

    start

---
+++ test/principal/TestTecnico.java
@@ -0,0 +1,14 @@
+package principal;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTecnico {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
